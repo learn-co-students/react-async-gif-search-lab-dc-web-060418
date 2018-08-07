0025a01095612c0c6a98cb017c95912433d869c6
orenmagid <autoimpedicis@gmail.com>
2018-08-07 15:02:39 -0400

    Done.

---
+++ src/components/GifSearch.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default class GifSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ""
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.fetchGIFs(this.state.query);
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.query}
+            onChange={event => this.setState({ query: event.target.value })}
+          />
+        </form>
+      </div>
+    );
+  }
+}
